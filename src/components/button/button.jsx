b3ad45408df30820ae129138914a49b29ca9ed1f
carmenvdheide <carmenvdheide@gmail.com>
2024-01-16 23:07:49 +0100

    made a working surprise button and buttons for the mealtypes

---
+++ src/components/button/button.jsx
@@ -0,0 +1,14 @@
+import "./button.css"
+
+function Button(props) {
+    return (
+        <button
+            onClick={props.onClick}
+            className={props.className}
+            value={props.value}>
+            {props.text}
+        </button>
+    )
+}
+
+export default Button
--- src/pages/random-recipe/random-recipe.jsx
+++ src/pages/random-recipe/random-recipe.jsx
@@ -1,7 +1,78 @@
+import Button from "../../components/button/button.jsx";
+import {useState} from "react";
+import axios from "axios";
+
 function RandomRecipe() {
+    const [ surpriseButtonText, setSurpriseButtonText ] = useState('surprise me')
+    const [ stateMealType, setStateMealType ] = useState(null)
+    const [ randomRecipeData, setRandomRecipeData] = useState(null)
+
+    function handleRandomButton() {
+        setSurpriseButtonText('nope, next')
+
+
+        async function fetchRandomRecipe() {
+            try {
+                const result = await axios.get('https://api.edamam.com/api/recipes/v2?app_id=5512310a&app_key=efdf28b15f81638625269787d80913f7&q=a&type=public', { params: {
+                        mealType: stateMealType,
+                        dishType: "main course",
+                        random: true,
+                    }})
+                console.log(result.data.hits[0])
+                setRandomRecipeData(result.data.hits[0])
+
+            } catch (e) {
+                console.error(e)
+                console.log('nope')
+            }
+        }
+        return fetchRandomRecipe()
+    }
+
+
+    function handleMealType(e) {
+        setStateMealType(e.target.value)
+        console.log(e.target.value)
+    }
+
+
     return (
         <>
-            <p>random</p>
+            <section>
+                <div>
+                    <Button
+                        text="breakfast"
+                        className="mealType"
+                        onClick={handleMealType}
+                        value="Breakfast"/>
+                    <Button
+                        text="lunch"
+                        className="mealType"
+                        onClick={handleMealType}
+                        value="Lunch"/>
+                    <Button
+                        text="dinner"
+                        className="mealType"
+                        onClick={handleMealType}
+                        value="Dinner"/>
+                </div>
+
+                <Button
+                    text={surpriseButtonText}
+                    className="surpriseButton"
+                    onClick={handleRandomButton}/>
+            </section>
+
+            {randomRecipeData ?
+                <section>
+                    <p>{randomRecipeData.recipe.label}</p>
+                    <p>{randomRecipeData.recipe.mealType}</p>
+                    <p>{randomRecipeData.recipe.dishType}</p>
+                </section>
+
+            : ""}
+
+
         </>
     )
 }
